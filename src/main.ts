941140e5a10478f66c8831d857fd4bed2652000d
yonghanjung <dydgks789@naver.com>
2024-11-25 11:35:26 +0900

    feat(store) - 전역 상태 관리를 위한 pinia 설치

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
-import { createApp } from 'vue'
-import './assets/style.css'
-import App from './App.vue'
-import router from './router';
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import ConfirmationService from "primevue/confirmationservice";
-import ToastService from "primevue/toastservice";
+import { createApp } from "vue";
+import "./assets/style.css";
+import App from "./App.vue";
+import router from "./router";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
-
-app.use(router).use(PrimeVue, {
+const pinia = createPinia();
+app
+  .use(router)
+  .use(PrimeVue, {
     theme: {
-        preset: Aura
-    }
-})
-    .use(ConfirmationService).use(ToastService)
-    .mount('#app');
+      preset: Aura,
+    },
+  })
+  .use(pinia)
+  .mount("#app");
